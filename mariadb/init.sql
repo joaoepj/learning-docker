2c1af9a219bfc82655569e8d8e22130c73a511ce
Joao E. Pereira Jr <joaoepj@gmail.com>
2023-03-16 16:49:52 -0300

    now coredns resolves docker container names and forwards ufu to ufu servers

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -17,16 +17,19 @@ CREATE TABLE IF NOT EXISTS `coredns_records` (
 INSERT INTO coredns_records (zone, name, ttl, content, record_type) VALUES
 ('example.org.', 'foo', 30, '{"ip": "1.1.1.1"}', 'A'),
 ('example.org.', 'foo', 60, '{"ip": "1.1.1.0"}', 'A'),
+('example.org.', 'bar', 30, '{"ip": "3.3.3.1"}', 'A'),
+('example.org.', 'bar', 60, '{"ip": "3.3.3.0"}', 'A'),
+
 ('example.org.', 'foo', 30, '{"text": "hello"}', 'TXT'),
 ('example.org.', 'foo', 30, '{"host" : "foo.example.org.","priority" : 10}', 'MX'),
-('ufu.br.', 'joao', 30, '{"ip": "2.2.2.0"}', 'A'),
-('ufu.br.', 'www', 30, '{"ip": "2.2.2.1"}', 'A'),
-('ufu.br.', 'ns1', 30, '{"ip": "2.2.2.3"}', 'A'),
-('ufu.br.', 'ns2', 30, '{"ip": "2.2.2.4"}', 'A'),
-('ufu.br.', 'smtp', 30, '{"ip": "2.2.2.5"}', 'A'),
-('ufu.br.', 'web', 30, '{"host": "www.ufu.br."}', 'CNAME'),
-('ufu.br.', 'ns1', 30, '{"host": "ns1.ufu.br."}', 'NS'),
-('ufu.br.', 'ns2', 30, '{"host": "ns2.ufu.br."}', 'NS'),
+('example.org.', 'joao', 30, '{"ip": "2.2.2.0"}', 'A'),
+('example.org.', 'www', 30, '{"ip": "2.2.2.1"}', 'A'),
+('example.org.', 'ns1', 30, '{"ip": "2.2.2.3"}', 'A'),
+('example.org.', 'ns2', 30, '{"ip": "2.2.2.4"}', 'A'),
+('example.org.', 'smtp', 30, '{"ip": "2.2.2.5"}', 'A'),
+('example.org.', 'web', 30, '{"host": "www.example.org."}', 'CNAME'),
+('example.org.', 'ns1', 30, '{"host": "ns1.example.org."}', 'NS'),
+('example.org.', 'ns2', 30, '{"host": "ns2.example.org."}', 'NS'),
 
-('ufu.br.', 'ufu.br', 30, '{"ns": "ns1.ufu.br.", "Mbox": "joao.ufu.br", "refresh": 7200, "retry": 3600, "expire": 21600, "minttl": 3600}', 'SOA'),
-('ufu.br.', 'dns', 30, '{"host": "ns1.ufu.br."}', 'CNAME');
+('example.org.', 'example.org', 30, '{"ns": "ns1.example.org.", "Mbox": "joao.example.org", "refresh": 7200, "retry": 3600, "expire": 21600, "minttl": 3600}', 'SOA'),
+('example.org.', 'dns', 30, '{"host": "ns1.example.org."}', 'CNAME');
