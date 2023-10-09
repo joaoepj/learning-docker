57cf4b55010f85347e9834178df4ec23db1086aa
Joao E Pereira Jr <joaoepj@gmail.com>
2023-10-09 11:29:02 -0300

    Tuning joaoepj-kea Dockerfile

--- node-http-proxy/index.js
+++ node-http-proxy/index.js
@@ -1,27 +1,23 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var httpProxy = require('http-proxy');
 
 //
 // Create a proxy server with latency
 //
-var proxy = httpProxy.createProxyServer();
-
-//
-// Create your server that makes an operation that waits a while
-// and then proxies the request
-//
-http.createServer(function (req, res) {
-        proxy.web(req, res, {
+var proxy = httpProxy.createProxyServer(
+        {
             target: 'http://200.19.145.141:8000'
-        });
-        
-        
-}).listen(7000);
+        }
+    ).listen(7000);
 
 proxy.on('proxyRes', function (req, res, proxyRes) {
+    console.log('Setting Outgoing Respose Access-Control-Allow-Origin Header...')
     proxyRes.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(JSON.stringify(res.headers, true, 2))
 });
 
 proxy.on('proxyReq', function (req, res, proxyReq) {
-    proxyReq.setHeader('User-Agent', '');
+    console.log('Setting Incoming Request Content-Type Header...')
+    proxyReq.setHeader('Content-Type', 'application/json');
+    console.log(JSON.stringify(req.headers, null, 2))
+    console.log(JSON.stringify(req.rawBody, null, 2))
 });
