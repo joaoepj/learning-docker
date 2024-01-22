c12149e30d90e8a5798fe0e809f595beb39313a1
Joao E Pereira Jr <joaoepj@gmail.com>
2024-01-22 19:27:56 -0300

    Improvements on Alisson to Kea converter

--- kea-dhcp/server/alisson2kea.js
+++ kea-dhcp/server/alisson2kea.js
@@ -1,36 +1,90 @@
-// in case of module not found error run the command below
-// npm install node-json-transform --save
+// in case of module not found error
+// make sure modules are installed into
+// project directory
 var transform = require("node-json-transform").transform;
 var fs = require("fs");
+const { http } = require("http");
 
 
+//let rawdata = fs.readFileSync("/root/first-subnet-dhcpapi.json");
 let rawdata = fs.readFileSync("/root/dhcpapi.json");
 let alisson = JSON.parse(rawdata);
 
-var map = {
+
+var Subnet4Map = {
+    item: {
+    subnet4: "results",
+    },
+    defaults: {
+        loggers: "/root/logs.log"
+    },
+    operate: [
+        {
+         run: function(val) { return  transform(val, SubnetsMap)}, on: "subnet4" 
+        },
+     ],
+    each: function(item){
+        return item; 
+    } 
+}
+
+var SubnetsMap = {
     item: {
         subnet: "id",
-        iprange: {
-        startip: "ip-range.0.start-ip",
-        endip: "ip-range.0.end-ip",
-        },
+        iprange:  {
+                startip: "ip-range.0.start-ip",
+                endip: "ip-range.0.end-ip",
+            },
+        options: "options",
         domain: "domain",
         netmask: "netmask",
-        pool: "pool"
-        
+        pool: "pool",
+        reservations: "reserved-address"
     },
     remove: ["domain"],
     operate: [
- //       {
- //          run: function() { return startip + '-' + endip}, on: pool 
- //       },
+       {
+          run: function(val) { return  val.startip + '-' + val.endip}, on: "iprange" 
+       },
+       {
+        run: function(val) { return  transform(val, ReservationsMap)}, on: "reservations" 
+       },
+       {
+        run: function(val) { return  transform(val, OptionsMap)}, on: "options" 
+       },       
+
     ],
     each: function(item){
         return item; 
     }
 }
 
-//var result = transform({"id": "10.0.0.0", "domain": "ufu.br"}, map);
-var result = transform(alisson.results, map);
+var ReservationsMap = {
+    item: {
+        description: "description",
+        "hw-address": "mac",
+        "ip-address": "ip"
+    },
+    each: function(item){
+        return item; 
+    }
+}
 
-console.log(result)
+var OptionsMap = {
+    item: {
+        option: "id",
+        value: "ip"
+    }
+}
+
+var PoolsMap = {
+    item: {
+        iprange: "iprange"
+    }
+}
+
+
+var result = transform(alisson, Subnet4Map);
+
+//console.log(JSON.stringify(result, null, 2))
+console.log(JSON.stringify(result))
